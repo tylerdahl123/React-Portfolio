9c382d0651613717016a7fe6aef66ae1554b0d39
tylerdahl123 <dahlgren15@gmail.com>
2021-01-25 21:09:48 -0800

    should be up to date now

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,33 +1,37 @@
 import React from "react";
-import {Navbar, NavDropdown, FormControl, Button, Form} from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 import 'bootstrap/dist/css/bootstrap.min.css';
+//when i used the react-boostrap navbar it would crash my app or just load and never display...this was the only work around i could find with projects and everything going on. 
+//im sorry
 function Nav(){
     return(
-        
-
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="https://github.com/tylerdahl123">Tyler Dahlgren</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedConten" aria-controls="navbarSupportedConten" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedConten">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="contact.html">Contact Me<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="portfolio.html">Portfolio</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="index.html">Home/About</a>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-          </form>
-        </div>
-      </nav>
-</header>
+      <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="https://github.com/tylerdahl123">Tyler Dahlgren</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedConten" aria-controls="navbarSupportedConten" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedConten">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="contact.html">Contact Me<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="portfolio.html">Portfolio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home/About</a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+            </form>
+          </div>
+        </nav>
+  </header>
 
 )
 }
